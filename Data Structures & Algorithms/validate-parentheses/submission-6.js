48c6fcbaeea3980f390ee0237c903fa0d0efbe20
Krutika Dhakate <129934429+Krutika529@users.noreply.github.com>
2026-05-24 00:36:32 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        let map = { '}': '{', ')' : '(', ']': '['};
+        if(s.length == 1) {
+            return false;
+        }
+        for(let i=0; i<s.length; i++) {
+            if(s[i] == '{' || s[i] === '[' || s[i] === '(') {
+                stack.push(s[i]);
+            }
+            else{
+                if(stack.pop() !== map[s[i]]) 
+                return false;
+            }
+        }
+        return stack.length === 0;
+    }
+}
